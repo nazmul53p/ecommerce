00a3721d521557adcba8b08e62e1f84e5ded3a47
nazmul-sslwireless <nazmul.haque@sslwireless.com>
2024-06-03 15:44:09 +0600

    fix

--- server/src/user/dto/index.ts
+++ server/src/user/dto/index.ts
@@ -1,13 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 // src/user/dto/create-user.dto.ts
 export class CreateUserDto {
+  @ApiProperty()
   readonly name: string;
+  @ApiProperty()
   readonly email: string;
+  @ApiProperty()
   readonly password: string;
 }
 
 // src/user/dto/update-user.dto.ts
 export class UpdateUserDto {
+  @ApiProperty()
   readonly name?: string;
+  @ApiProperty()
   readonly email?: string;
+  @ApiProperty()
   readonly password?: string;
 }
